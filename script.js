8ecf332256c2981f87084aacb878271dd6042a91
rakibul258862-crypto <rakibul258862@gmail.com>
2026-02-23 01:30:16 +0600

    get all count btn with id

---
+++ script.js
@@ -0,0 +1,7 @@
+let interviewList =[];
+let rejectedList =[];
+
+// get all count btn 
+let totalCount = document.getElementById('total');
+let interviewCount = document.getElementById('interview');
+let rejectedCount = document.getElementById('rejected');
