8926a3f444d95a789a2d11f8909c3323369e0507
rakibul258862-crypto <rakibul258862@gmail.com>
2026-02-23 01:47:01 +0600

    get all filter btn and selected main section by queryselector

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ let rejectedList =[];
 let totalCount = document.getElementById('total');
 let interviewCount = document.getElementById('interview');
 let rejectedCount = document.getElementById('rejected');
+
+// get all filter btn id 
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectFilterBtn = document.getElementById('rejected-filter-btn');
+
+const allCardSection = document.getElementById('all-cards');
+
+// console.log(allCardSection.children.length);
+// selected main section by queryselector
+    const mainContainer = document.querySelector('main');
+    const filterSection = document.getElementById('filtered-section');
+    const filterSectionR = document.getElementById('filtered-section-2');
