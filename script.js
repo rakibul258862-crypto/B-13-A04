f898f6385cfe54f21b27c1cee348bb0cf56284c4
rakibul258862-crypto <rakibul258862@gmail.com>
2026-02-23 01:50:37 +0600

    calculated function and toggles funciton add js

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ const allCardSection = document.getElementById('all-cards');
     const mainContainer = document.querySelector('main');
     const filterSection = document.getElementById('filtered-section');
     const filterSectionR = document.getElementById('filtered-section-2');
+    function calculateCount(){
+    totalCount.innerText = allCardSection.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+calculateCount()
+
+function toggleStyle(id){
+    // remove the color
+    allFilterBtn.classList.remove('btn-soft','text-white');
+    interviewFilterBtn.classList.remove('btn-soft','text-white');
+    rejectFilterBtn.classList.remove('btn-soft','text-white');
+    // add bg-color and previous color remove
+    allFilterBtn.classList.add('bg-primary','text-black');
+    interviewFilterBtn.classList.add('bg-primary', 'text-black');
+    rejectFilterBtn.classList.add('bg-primary','text-black');
+    // console.log(id);
+    // selected btn
+    const selected = document.getElementById(id);
